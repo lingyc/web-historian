8d1644bc5d63088b246ce585f3584ec6a6ea0eac
lingyc <lingchen215@gmail.com>
2016-06-29 15:16:17 -0700

    Serve static files on / access

--- web/request-handler.js
+++ web/request-handler.js
@@ -2,10 +2,23 @@ var path = require('path');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
 var httpHelpers = require('./http-helpers');
+var headers = httpHelpers.headers;
 
 
 
 exports.handleRequest = function (req, res) {
   // res.end(archive.paths.list);
-  httpHelpers.serveAssets ( res, 'web/public/index.html', content => res.end (content) );
+  console.log ('Making request type', req.method, 'at path:', req.url );
+  
+  if ( req.url === '/' ) {
+    res.writeHead(200, headers);
+    httpHelpers.serveAssets ( res, 'web/public/index.html', content => res.end (content) );
+  } else if ( req.url === '/styles.css') {
+    headers['Content-Type'] = 'text/css';
+    res.writeHead(200, headers);
+    httpHelpers.serveAssets ( res, 'web/public/styles.css', content => res.end (content) );
+  } else {
+    // ERROR
+    console.log( 'error' );
+  }
 };
