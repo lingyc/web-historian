9377584fedc417000c2b68abaab109aa835328ee
lingyc <lingchen215@gmail.com>
2016-06-30 11:46:53 -0700

    Passing all tests

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
-
+var http = require('http');
 /*
  * You will need to reuse the same paths many times over in the course of this sprint.
  * Consider using the `paths` object below to store frequently used file paths. This way,
@@ -59,6 +59,37 @@ exports.isUrlArchived = function(url, callback) {
   fs.access( exports.paths.archivedSites + '/' + url, fs.F_OK, err => callback(!err));
 };
 
-exports.downloadUrls = function() {
+exports.downloadUrls = function( urls, callback ) {
+  urls.forEach ( url => {
+    exports.isUrlArchived ( url, exists => {
+      var options = {
+        hostname: url,
+        method: 'GET',
+        headers: {
+          'Content-Type': 'text/html'
+        }
+      };
+
+      var req = http.request( options, res => {
+        var data = '';
+        res.setEncoding('utf8');
+        res.on('data', chunk => data += chunk);
+        res.on('end', () => {
+          // Make a file for that URL
+          console.log(exports.paths.archivedSites);
+          fs.writeFile( exports.paths.archivedSites + '/' + url, data, 'utf8', err => {
+            if (err) {
+              throw err;
+            }
+            console.log('File written');
+          });
+        });
+      });
+
+      req.on('error', err => console.error ( err ));
+
+      req.end();
+    });
+  });
 };
 
